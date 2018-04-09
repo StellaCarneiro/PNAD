c0ea730b58d67627255aa5eb4d02d7f957d3e552
Stella.Carneiro <stella.carneiro@usp.br>
2018-04-08 21:23:50 -0300

    PNAD C - Download

---
+++ DownloadingPNADC.R
@@ -0,0 +1,18 @@
+.rs.restartR() ## Restart session to clean enviroment ##
+
+########################################################
+
+# Installing packages
+
+Sys.setlocale("LC_CTYPE", "pt_BR.UTF-8") ## Encoding ##
+
+install.packages( "convey" , repos = "http://cran.rstudio.com/" )
+
+install.packages( "srvyr" , repos = "http://cran.rstudio.com/" )
+
+# Getting libraries
+
+library(devtools)
+
+
+
